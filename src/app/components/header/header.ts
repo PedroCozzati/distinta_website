7229796b0ec6d73880870096022e5956943e174b
PedroCozzati <p.cozzati27@gmail.com>
2026-04-27 14:43:37 -0300

    Auto-generated commit

--- src/app/components/header/header.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-header',
-  templateUrl: './header.html',
-  standalone: true
-})
-export class HeaderComponent {}
--- src/app/components/hero/hero.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-hero',
-  templateUrl: './hero.html',
-  standalone: true
-})
-export class HeroComponent {}
